9319dd1acfa479fcf63c2367d4e233995f0731e3
FAL <falworks.contact@gmail.com>
2021-01-29 01:04:31 +0900

    feat: change default value of tsOutDir

--- src/build/paths.ts
+++ src/build/paths.ts
@@ -20,7 +20,7 @@ export type Paths = {
   dirs: {
     /** Defaults to `src`. */
     src?: string;
-    /** Defaults to `ts-out`. */
+    /** Defaults to `ts-out-tmp`. */
     tsOut?: string;
     /** Defaults to `dist`. */
     dist?: string;
@@ -37,7 +37,7 @@ export const preparePaths = (partialPaths: Paths): RequiredPaths => {
   const { dirs, filenames } = partialPaths;
 
   const srcDir = dirs.src || "src";
-  const tsOutDir = dirs.tsOut || "ts-out";
+  const tsOutDir = dirs.tsOut || "ts-out-tmp";
   const distDir = dirs.dist || "dist";
 
   const tsOutEntryFilename = filenames.srcEntry.replace(/\.ts(x?)$/, ".js$1");
